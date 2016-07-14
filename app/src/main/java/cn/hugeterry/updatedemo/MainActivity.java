398f1d6f197a740cc608b8fc3588d632ac75c005
admin <xuxt-314@qq.com>
2016-07-14 20:10:21 +0800

    更新Key

--- app/src/main/java/cn/hugeterry/updatedemo/MainActivity.java
+++ app/src/main/java/cn/hugeterry/updatedemo/MainActivity.java
@@ -3,6 +3,7 @@ package cn.hugeterry.updatedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.hugeterry.updatefun.Key;
 import cn.hugeterry.updatefun.UpdateFunGO;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Key.API_TOKEN = "";
+        Key.RELEASEID = "";
         new UpdateFunGO(this);
     }
 
