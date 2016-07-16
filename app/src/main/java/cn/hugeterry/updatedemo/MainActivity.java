e6a8f3a3b9ef7fb9707780678bd42a2b10939220
admin <xuxt-314@qq.com>
2016-07-16 19:13:44 +0800

    调整使用类

--- app/src/main/java/cn/hugeterry/updatedemo/MainActivity.java
+++ app/src/main/java/cn/hugeterry/updatedemo/MainActivity.java
@@ -3,7 +3,7 @@ package cn.hugeterry.updatedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import cn.hugeterry.updatefun.Key;
+import cn.hugeterry.updatefun.UpdateKey;
 import cn.hugeterry.updatefun.UpdateFunGO;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Key.API_TOKEN = "";
-        Key.RELEASEID = "";
+        //在http://fir.im/注册账号后获得的API_TOKEN
+        UpdateKey.API_TOKEN = "";
+        UpdateKey.RELEASE_ID = "578a11e7748aac01b7000039";
         new UpdateFunGO(this);
     }
 
