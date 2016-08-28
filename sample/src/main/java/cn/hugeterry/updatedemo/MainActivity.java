fc5971c55478d34ce798a35a7019ba4701ed0360
HugeTerry <xuxt-314@qq.com>
2016-08-28 22:19:51 +0800

    增加手动更新界面

--- sample/src/main/java/cn/hugeterry/updatedemo/MainActivity.java
+++ sample/src/main/java/cn/hugeterry/updatedemo/MainActivity.java
@@ -1,7 +1,9 @@
 package cn.hugeterry.updatedemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.hugeterry.updatefun.config.UpdateKey;
 import cn.hugeterry.updatefun.UpdateFunGO;
@@ -17,10 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //此处填上在http://fir.im/注册账号后获得的API_TOKEN以及APP的应用ID
         UpdateKey.API_TOKEN = "";
-        UpdateKey.APP_ID = "573ecefa748aac36f4000007";
+        UpdateKey.APP_ID = "578a11e7748aac01b7000039";
         //如果你想通过Dialog来进行下载，可以如下设置
-        //UpdateKey.DialogOrNotification=UpdateKey.WITH_DIALOG;
+//        UpdateKey.DialogOrNotification=UpdateKey.WITH_DIALOG;
         UpdateFunGO.init(this);
+    }
+
+    public void toUpdateView(View view){
+        //跳转至手动更新Activity,还在奋斗中
+        Intent intent=new Intent(this,ManualUpdateActivity.class);
+        startActivity(intent);
     }
 
     @Override
