15f7f10a62b6340e46676ad6bec6f3339291c778
HugeTerry <xuxt-314@qq.com>
2016-08-28 23:38:23 +0800

    添加手动更新资源

---
+++ sample/src/main/java/cn/hugeterry/updatedemo/ManualUpdateActivity.java
@@ -0,0 +1,42 @@
+package cn.hugeterry.updatedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by hugeterry(http://hugeterry.cn)
+ * Date: 16/8/28 21:52
+ */
+public class ManualUpdateActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manualupdate);
+
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_white_24dp);
+        getSupportActionBar().setTitle("关于");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
