a61c10bb2f6ae38ab8e4308a8edea6ed7a695c44
HugeTerry <xuxt-314@qq.com>
2016-08-31 00:07:58 +0800

    修改注释

--- sample/src/main/java/cn/hugeterry/updatedemo/ManualUpdateActivity.java
+++ sample/src/main/java/cn/hugeterry/updatedemo/ManualUpdateActivity.java
@@ -3,7 +3,6 @@ package cn.hugeterry.updatedemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
