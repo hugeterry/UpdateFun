c69b3c30523a5a1dbe319483a159f9d0dfe858a3
HugeTerry <xuxt-314@qq.com>
2017-01-09 15:45:28 +0800

    添加版本显示

--- updatefun/src/main/java/cn/hugeterry/updatefun/view/UpdateDialog.java
+++ updatefun/src/main/java/cn/hugeterry/updatefun/view/UpdateDialog.java
@@ -18,7 +18,7 @@ import cn.hugeterry.updatefun.R;
 public class UpdateDialog extends Activity {
 
     private TextView yes, no;
-    private TextView tv_changelog;
+    private TextView tv_version, tv_changelog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +27,10 @@ public class UpdateDialog extends Activity {
 
         yes = (TextView) findViewById(R.id.updatedialog_yes);
         no = (TextView) findViewById(R.id.updatedialog_no);
+        tv_version = (TextView) findViewById(R.id.title);
         tv_changelog = (TextView) findViewById(R.id.updatedialog_text_changelog);
 
+        tv_version.setText("发现新版本: V" + DownloadKey.version);
         tv_changelog.setText("更新日志：\n" + DownloadKey.changeLog);
 
         yes.setOnClickListener(new OnClickListener() {
