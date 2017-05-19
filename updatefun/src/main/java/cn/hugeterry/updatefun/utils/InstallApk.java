3d412a882e255422164320a673c78da76f234aaa
hugeterry <xuxt-314@qq.com>
2017-05-19 13:54:30 +0800

    修复bug 更新readme

--- updatefun/src/main/java/cn/hugeterry/updatefun/utils/InstallApk.java
+++ updatefun/src/main/java/cn/hugeterry/updatefun/utils/InstallApk.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.content.FileProvider;
+import android.util.Log;
 
 import java.io.File;
+
+import cn.hugeterry.updatefun.R;
 
 /**
  * Created by hugeterry(http://hugeterry.cn)
@@ -21,7 +24,7 @@ public class InstallApk {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Uri contentUri = FileProvider.getUriForFile(context, context.getPackageName()+".updatefunfileprovider", apkfile);
+            Uri contentUri = FileProvider.getUriForFile(context, context.getString(R.string.updatefun_provider_file_authorities), apkfile);
             intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
         } else {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
